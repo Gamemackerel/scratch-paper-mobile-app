25a39ab13fe733ae56c811aedb0da861c0b69dcc
abemiller <abraham.r.m@gmail.com>
2024-04-04 17:31:00 -0700

    playing with textInput

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput>Open up App.js to start working on your app!</TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
   },
 });
